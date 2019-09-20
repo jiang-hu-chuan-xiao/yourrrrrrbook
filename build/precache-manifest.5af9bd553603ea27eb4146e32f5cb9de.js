558a3223c15b2e3474b1f9f609f255d02449fbe7
Your Name <you@example.com>
2019-09-20 17:19:00 +0800

    first commit

--- build/precache-manifest.5af9bd553603ea27eb4146e32f5cb9de.js
+++ build/precache-manifest.5af9bd553603ea27eb4146e32f5cb9de.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "11c6d0c1b9493e395092995b0c8885b4",
+    "revision": "0707bcfc449dfcac3b591eedb0b16e21",
     "url": "./index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "./static/css/2.848db5fd.chunk.css"
   },
   {
-    "revision": "d2c645e073ff9abf435d",
+    "revision": "1683390e6eee37488ec1",
     "url": "./static/css/main.55794fa5.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "./static/js/2.f10ee56c.chunk.js"
   },
   {
-    "revision": "d2c645e073ff9abf435d",
-    "url": "./static/js/main.c939b635.chunk.js"
+    "revision": "1683390e6eee37488ec1",
+    "url": "./static/js/main.587e0f68.chunk.js"
   },
   {
     "revision": "8c97409f0ee389fe75da",
